a8a333c95d35ba57b4684ef947fae9bcc5b87d8d
ramandleela <ramandleela@gmail.com>
2017-02-04 22:34:04 -0700

    New algorithm that completes under 10 milliseconds

--- src/main/java/com/ecs/anagram/component/AnagramComponent.java
+++ src/main/java/com/ecs/anagram/component/AnagramComponent.java
@@ -4,8 +4,10 @@ import java.util.ArrayList;
 // Java
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
 import java.util.regex.Matcher;
@@ -49,29 +51,152 @@ public class AnagramComponent {
 	public void initialize() {
 		logger.info("fileName: " + fileName);
 		
-		/*
-		//Retreive dictionary from online
-		RestTemplate restTemplate = new RestTemplate();
-    	ResponseEntity<String> response = restTemplate.getForEntity("https://users.cs.duke.edu/~ola/ap/linuxwords", String.class);
-    	if(response != null && response.getStatusCode() == HttpStatus.OK) {
-			String ecsWords = response.getBody().toString();
-			logger.info("ecsWords: " + ecsWords);
-		} else {
-			logger.warn("getting ecsWords failed");
-		}
-    	*/
-		
 		// Build Dictionary Words from cached file based
 		dictionaryWords = readDictionary(fileName);
 		logger.info("Dictionary Words retrieved...");
 		//logger.info("Dictionary Words are:" + dictionaryWords);
 	}
 	
+	public BaseRecord findAnagrams(String searchWord) {
+		
+		logger.info("findAnagrams() searchWord " + searchWord);
+		long startTime = System.currentTimeMillis();
+		
+		// input validations
+		if(!validateSearchWord(searchWord)) {
+			this.baseRecord = new MessageRecord("Couldn't find word " + searchWord);
+			return baseRecord;
+		}
+		
+		// Get dictionary words with length same as searchWord
+		List<String> filteredList = getFilteredList(searchWord);		
+		if(filteredList.isEmpty()) {
+			logger.info("filteredList is empty");
+			this.baseRecord = new MessageRecord("Couldn't find word " + searchWord);
+			return baseRecord;
+		}
+		//logger.info("filteredList " + filteredList);
+		
+		// Find anagrams in the filtered list
+		List<String> resultList = findAnagramsInFilteredList(searchWord, filteredList);
+		if(resultList.isEmpty()) {
+			logger.info("Couldn't find word " + searchWord);
+			this.baseRecord = new MessageRecord("Couldn't find word " + searchWord);
+		} else {
+			logger.info("Anagrams found in Dictionary are:" + resultList);
+			this.baseRecord = new AnagramRecord(searchWord, resultList.toString());
+		}
+		logger.info("Time taken: " + (System.currentTimeMillis() - startTime) + " milliseconds");	
+		return this.baseRecord;
+	}
+	
+	// Utility method to find dictionary words of a given length
+	protected List<String> getFilteredList(String str) {
+		List<String> filteredList = new ArrayList<>();
+		List<String> listOfStrings = new ArrayList<>();		
+		listOfStrings = Arrays.asList(dictionaryWords.split(","));
+		int strLength = str.length();
+		for (String s : listOfStrings) {
+          if ((s.length() == strLength) && (0 != str.compareToIgnoreCase(s))) {
+        	  filteredList.add(s);
+          }
+      }
+      return filteredList;
+	}
+	
+	protected List<String> findAnagramsInFilteredList(String searchWord, List<String> filteredList){
+		List<String> foundList = new ArrayList<>();
+		HashMap<Character,Integer> map = new HashMap<Character,Integer>();          
+		for(int i = 0; i < searchWord.length(); i++){
+		   char c = searchWord.charAt(i);
+		   Integer val = map.get(new Character(c));
+		   if(val != null){
+		     map.put(c, new Integer(val + 1));
+		   }else{
+		     map.put(c,1);
+		   }
+		}
+		//logger.info("findAnagramsInFilteredList() input: " + searchWord);
+		//logger.info(map.toString());
+		
+        for(String filtered : filteredList) {
+			boolean found = true;
+			for(Map.Entry<Character,Integer> entry : map.entrySet()) {
+				Character key = entry.getKey();
+				Integer value = entry.getValue();
+				//logger.info(key.charValue() + " - " + value.intValue());
+				if (value.intValue() != countOccurrences(filtered.toLowerCase(), key.charValue())) {
+					found = false;
+					//logger.info(filtered + " not found");
+					break;
+				}
+			}
+			if(found) {
+				//logger.info("Adding " + filtered + " to foundList");
+				foundList.add(filtered);
+			}
+        }
+			
+        //logger.info(foundList.toString());
+		return foundList;
+	}
+	
+	// Utility method to count occurrences of a char in a given string
+	protected int countOccurrences(String str, char c)
+	{
+	    int count = 0;
+	    for(int i = 0; i < str.length(); i++)
+	    {
+	        if(str.charAt(i) == c) count++;
+	    }
+	    return count;
+	}
+	
+	// Optimizations to stop Search Words here to avoid calling recursive method 
+	// This versions uses Regex for alphabets for to validate
+	protected boolean validateSearchWord(String searchWord) {		
+		Pattern pattern = Pattern.compile("[A-Za-z]*");
+		Matcher matcher = pattern.matcher(searchWord);
+		return  matcher.matches() ? true: false;
+	}
+		
+	// Utility method to read dictionary words from a file
+	protected String readDictionary(String fileName) {
+		StringBuilder lines = new StringBuilder();
+		String line = null;
+			
+		try {
+			BufferedReader bufferedReader = new BufferedReader (new FileReader(fileName));
+			while ( (line = bufferedReader.readLine()) != null) {
+				lines.append(line);
+				lines.append(",");
+			}
+			bufferedReader.close();
+		} catch (FileNotFoundException ex) {
+			    ex.printStackTrace();
+			
+		} catch (IOException ex2) {
+			   ex2.printStackTrace();
+		}
+
+		//System.out.println(lines.toString());
+		return lines.toString();
+	}
+		
+	/*
 	// This is the service method to find valid anagrams
 	public BaseRecord findAnagrams(String searchWord) {
 		// Use TreeMap to achieve sorting
 		TreeSet <String> result = new TreeSet <String>();
+		List<String> listOfStrings = new ArrayList<>();		
+		listOfStrings = Arrays.asList(dictionaryWords.split(","));
+		Collections.sort(listOfStrings);
+		int maxLength = getMaxLength(listOfStrings);
 		
+		if(searchWord.length() > maxLength){
+			this.baseRecord = new MessageRecord("Couldn't find word " + searchWord);
+			return baseRecord;
+		}	
 		// input validations
 		if(!validateSearchWord(searchWord)) {
 			this.baseRecord = new MessageRecord("Couldn't find word " + searchWord);
@@ -82,10 +207,6 @@ public class AnagramComponent {
 			this.baseRecord = new BaseRecord();
 		}
 		
-		List<String> listOfStrings = new ArrayList<>();		
-		listOfStrings = Arrays.asList(dictionaryWords.split(","));
-		Collections.sort(listOfStrings);
-	
 		Set<String> anagrams = getAnagrams(searchWord);
 		logger.info("Anagrams are: " + anagrams);
 		
@@ -106,9 +227,10 @@ public class AnagramComponent {
 		return this.baseRecord;
 	}
 	
+	
 	// Optimizations to stop Search Words here to avoid calling recursive method 
 	// This versions uses Regex for alphabets for to validate
-	protected boolean validateSearchWord(String searchWord) {
+	protected boolean validateSearchWord(String searchWord) {		
 		Pattern pattern = Pattern.compile("[A-Za-z]*");
 		Matcher matcher = pattern.matcher(searchWord);
 		if(matcher.matches()) {
@@ -120,6 +242,7 @@ public class AnagramComponent {
 		}				
 	}
 	 
+	/*
 	// This method returns all possible anagrams or null
 	// This is a recursive method
 	protected Set<String> getAnagrams(String str) {
@@ -177,28 +300,19 @@ public class AnagramComponent {
 		} 
 	}
 	
-	// Utility method to read dictionary words from a file
-	protected String readDictionary(String fileName) {
-		StringBuilder lines = new StringBuilder();
-		String line = null;
-			
-		try {
-			BufferedReader bufferedReader = new BufferedReader (new FileReader(fileName));
-			while ( (line = bufferedReader.readLine()) != null) {
-				lines.append(line);
-				lines.append(",");
-			}
-			bufferedReader.close();
-		} catch (FileNotFoundException ex) {
-			    ex.printStackTrace();
-			
-		} catch (IOException ex2) {
-			   ex2.printStackTrace();
-		}
-
-		//System.out.println(lines.toString());
-		return lines.toString();
+	// Utility method to find the length of the LongestString in an Array
+	protected int getMaxLength(List<String> listOfStrings) {
+      int maxLength = 0;
+      for (String s : listOfStrings) {
+          if (s.length() > maxLength) {
+              maxLength = s.length();
+          }
+      }
+      return maxLength;
 	}
+	*/
+	
+
 }
 
 
